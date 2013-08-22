1842da374d50a99119d0874e23e93c0ae4fec7b4
Isabel Server <server@ezypzy.com>
2013-08-22 22:10:45 +0800

    test

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'temsuser', '3zyPzy', 'tems');
+$mysqli = new mysqli('localhost', 'temsuser', '3zyPzy', 'tems2');
 if (mysqli_connect_errno()) {
     printf("Can't connect to MySQL Server. Errorcode: %s\n", mysqli_connect_error());
     exit;
--- includes/cons.php
+++ includes/cons.php
@@ -1,4 +1,4 @@
 <?php
   $link = mysql_connect('localhost','temsuser','3zyPzy');
-  $mydb = mysql_select_db('tems',$link);
+  $mydb = mysql_select_db('tems2',$link);
 ?>

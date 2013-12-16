35c6283e3e8fa39d7c8973243dd46275b6367cce
Husaini <husaini@ezypzy.com>
2013-12-16 17:11:46 +0800

    edit category: default selected

--- editcat.php
+++ editcat.php
@@ -66,7 +66,7 @@
                     <?php
                         $q = mysql_query("select * from asset_class");
                         while ($row = mysql_fetch_assoc($q)) : ?>
-                            <option value="<?php echo $row['id'];?>"><?php echo $row['name']; ?></option>
+                            <option value="<?php echo $row['id'];?>" <?php echo ($row['id'] == $tq) ? 'selected="selected"':'';?>><?php echo $row['name']; ?></option>
                         <?php endwhile; ?>
                 </select>
             </p>

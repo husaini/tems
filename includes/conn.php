a5801d86b7bd90b553e02f48dbacf266194bf669
Isabel Server <server@ezypzy.com>
2013-12-16 16:26:37 +0800

    .

--- includes/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'temsuser', '3zyPzy', 'tems2');
-if (mysqli_connect_errno()) {
-    printf("Can't connect to MySQL Server. Errorcode: %s\n", mysqli_connect_error());
-    exit;
-}
-?>
